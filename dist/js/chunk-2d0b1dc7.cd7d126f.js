3fe12a66bc998e9c7690d66f2b239f3a17c5e0b5
Jiawei Zheng <zjwdnlm93716@gmail.com>
2020-11-21 15:05:46 +0800

    add build project

---
+++ dist/js/chunk-2d0b1dc7.cd7d126f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b1dc7"],{"223d":function(e,n,c){"use strict";c.r(n);var t=function(){var e=this,n=e.$createElement,c=e._self._c||n;return c("h4",[e._v("You are second page.")])},u=[],a=c("2877"),l={},o=Object(a["a"])(l,t,u,!1,null,null,null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d0b1dc7.cd7d126f.js.map
---
+++ dist/js/chunk-2d21a3d2.c75b59fc.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a3d2"],{bb51:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("h4",[n._v("You are in home.")])},a=[],c=t("2877"),l={},o=Object(c["a"])(l,u,a,!1,null,null,null);e["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d21a3d2.c75b59fc.js.map
